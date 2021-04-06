2b5b238a676a0ecbb32aaf8f7c14cd65711ffcfa
Hendrik Neumann <hendrik.neumann@digitaspixelpark.com>
2021-04-06 17:24:01 +0200

    chore(lascheln): improve overall look and behaviour a little

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,14 +1,15 @@
 'use strict';
 
 let activePlaceholder = 0;
+let phrase;
 const words = require('./words.json');
 const btnDie = document.getElementById('btnDie');
 const btnReset = document.getElementById('btnReset');
+const firstPlaceholder = document.getElementById('word0');
+const sharer = document.getElementById('sharer');
 
 function init() {
-  const firstPlaceholder = document.getElementById('word0');
-
-  firstPlaceholder.style.borderColor = 'green';
+  firstPlaceholder.classList.add('word--active');
   btnDie.disabled = false;
   btnDie.addEventListener('click', getWord, false);
   btnReset.addEventListener('click', reset, false);
@@ -20,17 +21,25 @@ function reset() {
 
   placeholders.map((placeholder) => {
     placeholder.textContent = '';
-    placeholder.style.minWidth = '6ch';
-    placeholder.style.borderBottom = '4px solid';
+    placeholder.classList.remove('word--solved', 'word--active');
   });
+
+  firstPlaceholder.classList.add('word--active');
+  btnDie.disabled = false;
+  sharer.classList.add('hidden');
 }
 
 function genderWords() {
-  console.info('Needs gendering');
+  document.getElementById('word0Prefix').textContent = 'ein';
+  document.getElementById('word1').textContent += 'r';
+  document.getElementById('word2').textContent += 'r';
 }
 
 function getWord() {
   const placeholder = document.getElementById('word' + activePlaceholder);
+  const nextPlaceholder = document.getElementById(
+    'word' + (activePlaceholder + 1)
+  );
   const die = Math.floor(Math.random() * 6);
   let word = words[activePlaceholder];
 
@@ -45,12 +54,19 @@ function getWord() {
       break;
   }
 
-  placeholder.style.minWidth = 'unset';
-  placeholder.style.borderBottom = '0';
+  placeholder.classList.remove('word--active');
+  placeholder.classList.add('word--solved');
+  // placeholder.style.minWidth = 'unset';
+  // placeholder.style.borderColor = 'transparent';
+
   if (activePlaceholder < words.length - 1) {
+    nextPlaceholder.classList.add('word--active');
     activePlaceholder++;
   } else {
+    phrase = document.getElementById('phrase').textContent;
+    console.log(phrase);
     btnDie.disabled = true;
+    // sharer.classList.remove('hidden');
   }
 }
 
