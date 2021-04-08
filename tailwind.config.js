a56eaa53886cb4da47a9af7852c783284eee45c2
Hendrik Neumann <hendrik.neumann@digitaspixelpark.com>
2021-04-08 11:08:08 +0200

    style(lascheln): adjust color palette and some minor tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,20 @@
 module.exports = {
   // mode: 'jit',
   purge: ['./src/**/*.{html,css,scss,js,jsx,ts,tsx,vue}', './dist/**/*.html'],
+  theme: {
+    colors: {
+      black: '#000',
+      gray: '#9ca3af',
+      white: '#fff',
+      transparent: 'transparent',
+      primary: '#c471ed',
+      secondary: '#f64f59',
+      tertiary: '#12c2e9',
+    },
+    extend: {
+      animation: {
+        'spin-once': 'spin 0.5s linear',
+      },
+    },
+  },
 };
